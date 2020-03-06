cd572507ae3e811ee97679f23199058a67f15e35
Deijah Price <deijahwilson1@gmail.com>
2020-03-06 16:02:59 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,9 +1,13 @@
 require 'pry'
 
 def start_game(player1, player2)
-  "Hello #{player1} & #{playar2}"
+ puts  "Hello #{player1} & #{player2}"
 end
+start_game(Deijah, Brandon)
 
-def play_game(player2)
-  "#{player1} is better than #{player2}"
+binding.pry
+
+def play_game(player1, player2)
+  puts "#{player1} is better than #{player2}"
 end
+play_game(Deijah, Brandon)
